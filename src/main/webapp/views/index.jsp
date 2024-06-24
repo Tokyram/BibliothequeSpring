6a3cca740c493c8d0cf436a71648fbbf0d6041da
Ismael-Raveloson <120553853+Ismael-Raveloson@users.noreply.github.com>
2024-06-24 15:09:52 +0300

    test Isma

---
+++ src/main/webapp/views/index.jsp
@@ -0,0 +1,1 @@
+<h1>Kimason'i lisany amzay</h1>
---
+++ src/test/java/com/example/BibliothequeSpring/BibliothequeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BibliothequeSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothequeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
